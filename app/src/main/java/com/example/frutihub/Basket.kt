b7091ea626cbd94e5372a61de13d22029036c33a
samuel54Solano <127533662+samuel54Solano@users.noreply.github.com>
2023-08-16 16:45:22 -0500

    diseño

--- app/src/main/java/com/example/frutihub/Basket.kt
+++ app/src/main/java/com/example/frutihub/Basket.kt
@@ -1,8 +1,11 @@
 package com.example.frutihub
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler.Value
+import android.view.animation.LinearInterpolator
 import com.example.frutihub.databinding.ActivityBasketBinding
 import com.example.frutihub.databinding.CompleteOrderBootomBinding
 import com.example.frutihub.databinding.DatosPersonalesBootomBinding
@@ -15,7 +18,133 @@ class Basket : AppCompatActivity() {
         binding = ActivityBasketBinding.inflate(layoutInflater)
         setContentView(binding.root)
         botoomSheep()
+        animations()
+        invicible()
     }
+
+    fun invicible() {
+        binding.apply {
+            quinoa.alpha = 0f
+            linea1.alpha = 0f
+            melon.alpha = 0f
+            linea2.alpha = 0f
+            tropical.alpha = 0f
+        }
+    }
+
+    fun animations() {
+        binding.apply {
+            //Contenedor
+            var inicioCont = contenedor.y
+            var finCont = contenedor.y + 2000
+            contenedor.y = finCont
+            var transtationCont = ValueAnimator.ofFloat(finCont, inicioCont)
+            transtationCont.duration = 500
+            transtationCont.interpolator = LinearInterpolator()
+            transtationCont.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                contenedor.translationY = animation
+            }
+            transtationCont.startDelay = 700
+            transtationCont.start()
+
+
+            //cart1
+            var apareceCart1 = ValueAnimator.ofFloat(0f, 1f)
+            apareceCart1.duration = 300
+            apareceCart1.interpolator = LinearInterpolator()
+            apareceCart1.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                quinoa.alpha = animation
+                linea1.alpha = animation
+            }
+
+
+            var inicioCart1 = quinoa.x
+            var finCart1 = quinoa.x - 2000
+            quinoa.x = finCart1
+            var translationCart1 = ValueAnimator.ofFloat(finCart1, inicioCart1)
+            translationCart1.duration = 1500
+            translationCart1.interpolator = LinearInterpolator()
+            translationCart1.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                quinoa.translationX = animation
+                linea1.translationX = animation
+            }
+            translationCart1.startDelay = 1400
+            translationCart1.start()
+
+            apareceCart1.startDelay = 1400
+            apareceCart1.start()
+
+
+            //cart2
+            var apareceCart2 = ValueAnimator.ofFloat(0f, 1f)
+            apareceCart2.duration = 300
+            apareceCart2.interpolator = LinearInterpolator()
+            apareceCart2.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                melon.alpha = animation
+                linea2.alpha = animation
+            }
+            var inicioCart2 = melon.x
+            var finCart2 = melon.x + 2000
+            melon.x = finCart2
+            var translationCart2 = ValueAnimator.ofFloat(finCart2, inicioCart2)
+            translationCart2.duration = 1500
+            translationCart2.interpolator = LinearInterpolator()
+            translationCart2.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                melon.translationX = animation
+                linea2.translationX = animation
+            }
+            translationCart2.startDelay = 1800
+            translationCart2.start()
+            apareceCart2.startDelay = 1800
+            apareceCart2.start()
+
+
+            //cart3
+            var apareceCart3 = ValueAnimator.ofFloat(0f, 1f)
+            apareceCart3.duration = 300
+            apareceCart3.interpolator = LinearInterpolator()
+            apareceCart3.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                tropical.alpha = animation
+            }
+
+            var inicioCart3 = tropical.x
+            var finCart3 = tropical.x - 2000
+            tropical.x = finCart3
+            var translationCart3 = ValueAnimator.ofFloat(finCart3, inicioCart3)
+            translationCart3.duration = 1500
+            translationCart3.interpolator = LinearInterpolator()
+            translationCart3.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                tropical.translationX = animation
+            }
+            translationCart3.startDelay = 2200
+            translationCart3.start()
+            apareceCart3.startDelay = 2200
+            apareceCart3.start()
+
+
+            //bootom
+            var inicioBootom = bootom.y
+            var finBootom = bootom.y + 2000
+            bootom.y = finBootom
+            var transtationBoottom = ValueAnimator.ofFloat(finBootom, inicioBootom)
+            transtationBoottom.duration = 500
+            transtationBoottom.interpolator = LinearInterpolator()
+            transtationBoottom.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                bootom.translationY = animation
+            }
+            transtationBoottom.startDelay = 3400
+            transtationBoottom.start()
+        }
+    }
+
 
     fun botoomSheep() {
         binding.back.setOnClickListener {
@@ -41,6 +170,37 @@ class Basket : AppCompatActivity() {
                         btnCompleteOrder.setOnClickListener {
                             startActivity(Intent(this@Basket, OrderComplete::class.java))
                         }
+
+                        txtNumbers.alpha = 0f
+                        txtDelivery.alpha = 0f
+                        btnDelivery.alpha = 0f
+                        btnCarPay.alpha = 0f
+
+
+                        var apareceTxt1 = ValueAnimator.ofFloat(0f, 1f)
+                        apareceTxt1.duration = 500
+                        apareceTxt1.interpolator = LinearInterpolator()
+                        apareceTxt1.addUpdateListener { ValueAnimator ->
+                            var animation = ValueAnimator.animatedValue as Float
+                            txtDelivery.alpha = animation
+                        }
+
+                        var iniciotxt1 = txtDelivery.y
+                        var fintxt1 = txtDelivery.y + 300
+                        txtDelivery.y = fintxt1
+                        var translacionTxt1 = ValueAnimator.ofFloat(fintxt1, iniciotxt1)
+                        translacionTxt1.duration = 600
+                        translacionTxt1.interpolator = LinearInterpolator()
+                        translacionTxt1.addUpdateListener { ValueAnimator ->
+                            var animation = ValueAnimator.animatedValue as Float
+                            txtDelivery.translationY = animation
+                        }
+                        translacionTxt1.startDelay = 400
+                        translacionTxt1.start()
+                        apareceTxt1.startDelay = 400
+                        apareceTxt1.start()
+
+
                     }
                 }
             }
--- app/src/main/java/com/example/frutihub/OrderComplete.kt
+++ app/src/main/java/com/example/frutihub/OrderComplete.kt
@@ -1,10 +1,12 @@
 package com.example.frutihub
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
 import com.example.frutihub.databinding.ActivityOrderCompleteBinding
-import com.example.frutihub.databinding.CompleteOrderBootomBinding
 
 class OrderComplete : AppCompatActivity() {
     lateinit var binding: ActivityOrderCompleteBinding
@@ -12,7 +14,69 @@ class OrderComplete : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOrderCompleteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Invisible()
+        Pasar()
+        Animation()
+    }
 
+    fun Invisible() {
+        binding.apply {
+            cheked.alpha = 0f
+            textos.alpha = 0f
+            btnTrack.alpha = 0f
+            btnContinueShopping.alpha = 0f
+        }
+    }
+
+    fun Animation() {
+        binding.apply {
+            //cheked
+            var apareceCheked = ValueAnimator.ofFloat(0f, 1f)
+            apareceCheked.duration = 1000
+            apareceCheked.interpolator = DecelerateInterpolator()
+            apareceCheked.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                cheked.alpha = animation
+                cheked.scaleY = animation
+                cheked.scaleX = animation
+            }
+
+            apareceCheked.startDelay = 1500
+            apareceCheked.start()
+
+
+            //textos
+            var apareceTextos = ValueAnimator.ofFloat(0f, 1f)
+            apareceTextos.duration = 1000
+            apareceTextos.interpolator = DecelerateInterpolator()
+            apareceTextos.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                textos.alpha = animation
+                textos.scaleY = animation
+                textos.scaleX = animation
+            }
+
+            var inicioTxt = textos.y
+            var finTxt = textos.y + 300
+            textos.y = finTxt
+            var transladarTexto = ValueAnimator.ofFloat(finTxt, inicioTxt)
+            transladarTexto.duration = 500
+            transladarTexto.interpolator = LinearInterpolator()
+            transladarTexto.addUpdateListener { ValueAnimator ->
+                var animation = ValueAnimator.animatedValue as Float
+                textos.translationY = animation
+            }
+
+            transladarTexto.startDelay = 2000
+            transladarTexto.start()
+            apareceTextos.startDelay = 2000
+            apareceTextos.start()
+
+        }
+    }
+
+
+    fun Pasar() {
         binding.apply {
             btnTrack.setOnClickListener {
                 startActivity(Intent(this@OrderComplete, Track::class.java))
@@ -23,6 +87,6 @@ class OrderComplete : AppCompatActivity() {
 
             }
         }
-
     }
+
 }
